3eccb00b8aca3f45ebe357e0897ce78ed8d6f3b0
George Marques <george@georgemarques.com.br>
2015-03-07 13:42:05 -0300

    feat(layout): add header with navbar

--- assets/components/app.jsx
+++ assets/components/app.jsx
@@ -1,10 +1,12 @@
 var React = require('react'),
+    HeaderComponent = require('./header.jsx'),
     AppComponent;
 
 AppComponent = React.createClass({
   render: function () {
     return (
       <div>
+        <HeaderComponent />
         {this.props.component && <this.props.component collections={this.props.collections} />}
       </div>
     );
