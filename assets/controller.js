0d62a82e7597128418331ec17dde36672a06e34e
George Marques <george@georgemarques.com.br>
2015-03-08 20:15:47 -0300

    feat(controller): add server bootstap

--- assets/controller.js
+++ assets/controller.js
@@ -1,8 +1,10 @@
 var Backbone = require('backbone'),
+    _ = require('underscore'),
     MainRouter = require('./routers/main'),
     fs = require('fs'),
     path = require('path'),
-    najax = require('../lib/najax');
+    najax = require('../lib/najax'),
+    bootstrap = require('./bootstrap');
 
 Backbone.ajax = function (options) {
   najax(options);
@@ -12,19 +14,45 @@ module.exports = {
   init: function (context) {
     return function (callback) {
       var mainRouter = new MainRouter();
-      mainRouter.on('route', function (route) {
+      mainRouter.on('route', function (routeName) {
         fs.readFile(path.join(process.cwd(), 'assets/index.html'), 'utf8', function (err, template) {
           if (err) { return callback(err); }
-          var rendered = this.render(template, mainRouter[route]().render());
-          callback(null, rendered);
+          var route = mainRouter[routeName]();
+          this.bootstrap(route, function (err) {
+            if (err) { return callback(err); }
+            var rendered = this.render(template, route.render());
+            callback(null, rendered);
+          }.bind(this));
         }.bind(this));
       }.bind(this));
       Backbone.history.loadUrl(context.req.url);
     }.bind(this);
   },
 
-  render: function (template, markup, data) {
+  bootstrap: function (route, callback) {
+    if (!route.collections || _.size(route.collections === 0)) {
+      callback(null);
+    }
+    var err = null, after = _.after(_.size(route.collections), function () {
+      callback(err, bootstrap);
+    });
+    _.each(route.collections, function (collection, name) {
+      var col = new collection();
+      col.fetch({
+        success: function () {
+          bootstrap[name] = col.toJSON();
+          after();
+        },
+        error: function (xhr, error, e) {
+          err = e;
+          after();
+        }
+      });
+    });
+  },
+
+  render: function (template, markup) {
     var rendered = template.replace('---Markup---', markup);
-    return rendered.replace('---BOOTSTRAP---', data || 'null');
+    return rendered.replace('---BOOTSTRAP---', JSON.stringify(bootstrap));
   }
 };
