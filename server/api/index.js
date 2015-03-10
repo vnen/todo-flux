fdc8be105a20378073733b1eb8a89410a4d2d3bc
George Marques <george@georgemarques.com.br>
2015-03-09 22:02:30 -0300

    feat(server): add 404 handler for api

--- server/api/index.js
+++ server/api/index.js
@@ -5,11 +5,19 @@ var api = require('koa')(),
 
 router.get('/todos', todos.getAll);
 router.get('/todos/:id', todos.getOne);
+router.delete('/todos/:id', todos.delete);
 
 api.use(auth);
 
 api.use(router.routes());
 api.use(router.allowedMethods());
+
+api.use(function *notFound(){
+  this.status = 404;
+  this.body = {
+    error: 'Page not found'
+  };
+});
 
 module.exports = {
   base: '/api',
